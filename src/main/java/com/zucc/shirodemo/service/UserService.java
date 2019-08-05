d24e50379fc70bb5ba419e3903cc5e96fc6edd0f
zhang-wangz <745268436@qq.com>
2019-08-05 17:20:43 +0800

    initt

---
+++ src/main/java/com/zucc/shirodemo/service/UserService.java
@@ -0,0 +1,28 @@
+package com.zucc.shirodemo.service;
+
+import com.zucc.shirodemo.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.Random;
+
+@Service
+public class UserService {
+
+    /**
+     * 模拟查询返回用户信息
+     * @param uname
+     * @return
+     */
+    public User findUserByName(String uname){
+        User user = new User();
+        user.setUname(uname);
+        user.setNick(uname+"NICK");
+        user.setPwd("J/ms7qTJtqmysekuY8/v1TAS+VKqXdH5sB7ulXZOWho=");//密码明文是123456
+        user.setSalt("wxKYXuTPST5SG0jMQzVPsg==");//加密密码的盐值
+        user.setUid(new Random().nextLong());//随机分配一个id
+        user.setCreated(new Date(System.currentTimeMillis()));
+
+        return user;
+    }
+}
